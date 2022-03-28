4b34ee6e155977037dbb7789475a17a5b1d5a502
physicsuser <gonz.reina@gmail.com>
2022-03-28 10:48:23 +0100

    testing it works

--- talk_to_port.py
+++ talk_to_port.py
@@ -1,11 +1,11 @@
-# from psychopy import parallel
-# from utils.braille_functions import toBraille
-# import numpy as np
-# import psychopy.core as core
-# import psychopy.visual
-# import psychopy.event
-# from psychopy import core
-# from psychopy import parallel 
+from psychopy import parallel
+from utils.braille_functions import sendStim, toBraille
+import numpy as np
+import psychopy.core as core
+import psychopy.visual
+import psychopy.event
+from psychopy import core
+from psychopy import parallel 
 
 # trigport = parallel.ParallelPort(address='0xDFF8')
 # FALSEL_trig = 3
@@ -33,17 +33,17 @@
 
 # win.close()
 
-# port = parallel.ParallelPort(address=0xDFF8)
+port = parallel.ParallelPort(address=0xDFF8)
 
 
-# pp = np.array(
-#     [   [1, 1, 0, 0, 1, 1, 0, 0],
-#         [0, 0, 1, 1, 0, 0, 1, 1],
-#         [0, 1, 1, 0, 0, 1, 1, 0],
-#         [1, 1, 1, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 1, 1, 1, 1],
-#     ]
-# )
+pp = np.array(
+    [   [1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1],
+        [0, 1, 1, 0, 0, 1, 1, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1],
+    ]
+)
 
 # g = np.random.gamma(4, 0.3, 10)
 # g = np.round(g)
@@ -51,7 +51,7 @@
     
 # r = np.random.randint(0,pp.shape[0])
 # true_patterns = 2
-# all_down = toBraille(np.zeros([1,8],dtype="int32"),0)
+all_down = toBraille(np.zeros([1,8],dtype="int32"),0)
 # stimuli = np.array([range(0,pp.shape[0])])
 # np.place(stimuli, stimuli==r, np.random.randint(0,5,5)) 
 # stimuli = np.random.permutation(stimuli)
@@ -60,22 +60,22 @@
     
 
 
-# seq = toBraille(pp[4,:],0)
-# sendStim(seq,port)
-# core.wait(10)
-# sendStim(all_down,port)
+seq = toBraille(pp[4,:],0)
+sendStim(seq,port)
+core.wait(10)
+sendStim(all_down,port)
 
 
-import numpy as np
-import codecs, json 
+# import numpy as np
+# import codecs, json 
 
-a = np.arange(5).reshape(1,5) # a 2 by 5 array
-b = a.tolist() # nested lists with same data, indices
-q = np.arange(10).reshape(2,5)
-q = q.tolist()
-data = {'fieldname':b,'another':q}
-file_path = "test.json" ## your path variable
-json.dump(data, codecs.open(file_path, 'w', encoding='utf-8'), 
-          separators=(',', ':'), 
-          sort_keys=True, 
-          indent=4) ### this saves the array in .json format
+# a = np.arange(5).reshape(1,5) # a 2 by 5 array
+# b = a.tolist() # nested lists with same data, indices
+# q = np.arange(10).reshape(2,5)
+# q = q.tolist()
+# data = {'fieldname':b,'another':q}
+# file_path = "test.json" ## your path variable
+# json.dump(data, codecs.open(file_path, 'w', encoding='utf-8'), 
+#           separators=(',', ':'), 
+#           sort_keys=True, 
+#           indent=4) ### this saves the array in .json format
